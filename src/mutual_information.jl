ce039f4d3488dbb5e701bae8efc49c19df53f8cc
Luc Rocher <luc@rocher.lc>
2018-02-12 16:35:22 +0000

    Remove old terminology

--- src/mutual_information.jl
+++ src/mutual_information.jl
@@ -9,34 +9,37 @@ function mi_freqs(probs::AbstractMatrix{Float64}; normalize::Bool=false)
 end
 
 
-function mutual_information{T<:Number}(x::AbstractVector{T}, y::AbstractVector{T},
-                                       ex, ey; params::CountsMI=CountsMI())
-  ee = joint_entropy(x, y; method=params.method)
+function mutual_information{T<:Number}(
+    x::AbstractVector{T}, y::AbstractVector{T}, ex, ey; method=:Naive)
+    ee = joint_entropy(x, y; method=:Naive, adjusted=false, normalize=false)
 
-  if params.adjusted
-    ee_shuffle = joint_entropy(x, shuffle(y); method=params.method)
-    mi_shuffle = ex + ey - ee_shuffle
-    mi = ee_shuffle - ee
-    params.normalize ? mi / (min(ex, ey) - mi_shuffle) : mi
-  else
-    mi = ex + ey - ee
-    params.normalize ? min(mi / min(ex, ey), 1.) : mi
-  end
+    if adjusted
+        ee_shuffle = joint_entropy(x, shuffle(y); method=method)
+        mi_shuffle = ex + ey - ee_shuffle
+        mi = ee_shuffle - ee
+        normalize ? mi / (min(ex, ey) - mi_shuffle) : mi
+    else
+        mi = ex + ey - ee
+        normalize ? min(mi / min(ex, ey), 1.) : mi
+    end
 end
 
-function mutual_information{T<:Number}(x::AbstractVector{T}, y::AbstractVector{T},
-                                       ; params::CountsMI=CountsMI())
-  ex = entropy_estimator(x)
-  ey = entropy_estimator(y)
-  mutual_information(x, y, ex, ey; params=params)
+function mutual_information{T<:Number}(
+    x::AbstractVector{T}, y::AbstractVector{T}; method=:Naive, adjusted=false, normalize=false)
+
+    ex = entropy_estimator(x)
+    ey = entropy_estimator(y)
+    mutual_information(x, y, ex, ey; method=method, adjusted=adjusted, normalize=normalize)
 end
 
-function mutual_information(data::AbstractMatrix; params::CountsMI=CountsMI())
+function mutual_information(
+    data::AbstractMatrix; method=:Naive, adjusted=false, normalize=false)
+
     M = size(data, 2)
     mi_sym = Array{Float64}(M, M)
 
     for i =1:M
-      mi_sym[i,i] = entropy_estimator(@view data[:,i]; method=params.method)
+        mi_sym[i,i] = entropy_estimator(@view data[:,i]; method=method)
     end
 
     for i = 1:M, j=i+1:M
@@ -46,7 +49,7 @@ function mutual_information(data::AbstractMatrix; params::CountsMI=CountsMI())
         x = @view data[:,i]
         y = @view data[:,j]
 
-        mi_sym[i, j] = mutual_information(x, y, ex, ey; params=params)
+        mi_sym[i, j] = mutual_information(x, y, ex, ey; method=method, adjusted=adjusted, normalize=normalize)
 
         # Drop null or negative values
         if mi_sym[i, j] < Base.eps()
@@ -56,12 +59,4 @@ function mutual_information(data::AbstractMatrix; params::CountsMI=CountsMI())
         mi_sym[j,i] = mi_sym[i,j]  # tbr
     end
     return Symmetric(mi_sym)
-end
-
-function mutual_information(p::SolidPrior; params::CountsMI=CountsMI())
-  mutual_information(p.data; params=params)
-end
-
-function mutual_information(p::PairwisePrior)
-  p.mi_matrix
 end
