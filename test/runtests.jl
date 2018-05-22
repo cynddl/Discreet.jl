250224bdf2d0c2026df817bf8f9b85f2b9e646ed
Luc Rocher <luc@rocher.lc>
2018-05-22 11:22:29 +0200

    Add tests for entropy routines

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,28 @@ import StatsBase: FrequencyWeights, ProbabilityWeights
 using Base.Test
 
 
-# Naive estimator
-@test 0 == entropy([1,1,1,1,1,1])
-@test -log(1/6) ≈ entropy(FrequencyWeights([1,1,1,1,1,1]))
+# Tests for entropy routines
+
+const freqs_uniform = FrequencyWeights([1, 1, 1, 1, 1, 1])
+const probs_uniform = ProbabilityWeights([.5, .5])
+const sample = [:a, :b, :c, :d, :e, :f]
+
+## Naive estimator
+@test log(6) ≈ entropy(freqs_uniform)
 @test 0 == entropy(FrequencyWeights([6]))
+@test log(6) ≈ estimate_entropy(sample)
 
-
-# From library(entropy) in R
+## CS and shrinkage estimates, from library(entropy) in R
 @test 3.840549310406 ≈ entropy(FrequencyWeights([1,1,1,1,1,1]); method=:CS)
 @test 2.201137101279 ≈ entropy(FrequencyWeights([4,2,3,2,4,2,1,1]); method=:CS)
 @test 1.968382408728 ≈ entropy(FrequencyWeights([4,2,3,2,4,2,1,1]))
-@test 2.379602895309 ≈ entropy(
-    FrequencyWeights([4,2,3,0,2,4,0,0,2,1,1]);
-    method=:Shrink)
+@test 2.379602895309 ≈ entropy(FrequencyWeights([4,2,3,0,2,4,0,0,2,1,1]);
+                               method=:Shrink)
+
+## Any other method
+@test_throws ArgumentError entropy(freqs_uniform; method=:NotImplemented)
+@test_throws ArgumentError estimate_entropy(sample; method=:NotImplemented)
+
+## Joint entropy
+@test log(6) ≈ estimate_joint_entropy(sample, sample)
+@test 3.840549310406 ≈ estimate_joint_entropy(sample, sample; method=:CS)
